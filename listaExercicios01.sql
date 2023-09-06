a611e9d400053d6aa559004024a0b7cc9438f344
guihub00 <102246436+guihub00@users.noreply.github.com>
2023-09-06 17:44:44 -0300

    Update listaExercicios01.sql
    ex 15

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -71,6 +71,11 @@ SELECT matriculas.curso, alunos.nome
 FROM matriculas
 RIGHT JOIN alunos ON matriculas.aluno_id = alunos.id;
 
+exercício 15 
+SELECT alunos.nome, matriculas.curso
+FROM alunos
+INNER JOIN matriculas ON alunos.id = matriculas.aluno_id;
+
 
 
 
