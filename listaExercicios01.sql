920754f460d0d9c9342d9c17bf63edd5e4304595
guihub00 <102246436+guihub00@users.noreply.github.com>
2023-09-06 17:41:17 -0300

    Update listaExercicios01.sql
    ex 9

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -38,3 +38,10 @@ SELECT produto, AVG(receita) AS media_receita
 FROM vendas
 GROUP BY produto;
 
+exercício 9
+SELECT produto, SUM(receita) AS receita_total
+FROM vendas
+GROUP BY produto
+HAVING SUM(receita) > 10000;
+
+
