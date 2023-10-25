12621c3483de750e5be88edf98c3acdea3948498
guihub00 <102246436+guihub00@users.noreply.github.com>
2023-10-25 18:14:36 -0300

    2

--- exerciciosDeFixacao20231025.sql
+++ exerciciosDeFixacao20231025.sql
@@ -5,3 +5,11 @@ for each row
 
 insert into Auditoria (mensagem)
 values ('Novo cliente');
+
+exerc 2 
+create trigger insert_exc_clientes before insert 
+on Clientes 
+for each row
+
+insert into Auditoria (mensagem)
+values ('Tentativa de exclusão');
