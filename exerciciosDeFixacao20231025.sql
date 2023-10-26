e7ff530c24c37765cefcfffd1ea38a80182ecabb
guihub00 <102246436+guihub00@users.noreply.github.com>
2023-10-25 23:04:45 -0300

    3

--- exerciciosDeFixacao20231025.sql
+++ exerciciosDeFixacao20231025.sql
@@ -13,3 +13,18 @@ for each row
 
 insert into Auditoria (mensagem)
 values ('Tentativa de exclusão');
+
+exerc 3 
+create trigger Nome_Cliente_Atualizado after update
+on Clientes 
+for each row
+begin
+    declare E nome_antigo VARCHAR(255);
+    set nome_antigo = OLD.nome;
+    declare E nome_novo VARCHAR(255);
+    set nome_novo = new.nome;
+
+    insert into Auditoria (mensagem) 
+    values ('Nome Antigo',nome_antigo,'para', 'Nome Novo', nome_novo);
+end;
+
