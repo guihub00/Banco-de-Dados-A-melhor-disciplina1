6f15ecbb1d11bbcc4f4a4da269942f51b0ba732a
guihub00 <102246436+guihub00@users.noreply.github.com>
2023-10-25 23:08:17 -0300

    4

--- exerciciosDeFixacao20231025.sql
+++ exerciciosDeFixacao20231025.sql
@@ -19,12 +19,31 @@ create trigger Nome_Cliente_Atualizado after update
 on Clientes 
 for each row
 begin
-    declare E nome_antigo VARCHAR(255);
+    declare E nome_antigo VARCHAR(150);
     set nome_antigo = OLD.nome;
-    declare E nome_novo VARCHAR(255);
+    declare E nome_novo VARCHAR(150);
     set nome_novo = new.nome;
 
     insert into Auditoria (mensagem) 
     values ('Nome Antigo',nome_antigo,'para', 'Nome Novo', nome_novo);
 end;
+
+exerc 4 
+use exercicios_trigger;
+
+delimiter $$
+
+create trigger impedir_Vazio 
+before update 
+on Clientes 
+for each row
+begin
+    if NEW.nome is null or NEW.nome = '' then
+        set NEW.nome = OLD.nome;
+        insert into Auditoria (mensagem) values ('Uma tentativa de atualização de nome para um valor vazio ou NULL é inválida.');
+    end if;
+end;
+$$
+
+delimiter ;
 
